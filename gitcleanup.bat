10ee52ecb03ea2d070f5911e539134a70b5b5709
TVA018 <thomas.vu018@gmail.com>
2026-02-10 21:12:29 -0500

    Update gitcleanup.bat

--- gitcleanup.bat
+++ gitcleanup.bat
@@ -1,10 +1,13 @@
 @echo off
 git fetch --prune
+
+echo Are you sure? Press any key after this to go through with the deletion
+pause
+
 for /f "delims=" %%b in ('git branch --format="%%(refname:short)" ^| findstr /v "^main$"') do (
     git ls-remote --exit-code --heads origin %%b >nul 2>&1
+
     if errorlevel 1 (
         git branch -D %%b
     )
 )
-pause
-
